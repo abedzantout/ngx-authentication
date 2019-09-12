3f4ac19e2b259a2007524f048c25e83785184761
Abed Zantout <abed@techhive.io>
2019-09-12 16:53:17 +0300

    feat(app): added auth-container skeleton

--- projects/ngx-authentication/src/lib/ngx-authentication.module.ts
+++ projects/ngx-authentication/src/lib/ngx-authentication.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
-import { NgxAuthenticationComponent } from './ngx-authentication.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
+import { NgxAuthenticationComponent } from './ngx-authentication.component';
+import { AuthContainerComponent } from './containers/auth-container/auth-container.component';
+
+const components = [NgxAuthenticationComponent, AuthContainerComponent];
+const services = [];
+const entryComponents = [];
 @NgModule({
-  declarations: [NgxAuthenticationComponent],
-  imports: [],
-  exports: [NgxAuthenticationComponent]
+  imports: [CommonModule, RouterModule, FormsModule],
+  declarations: [...components, ...entryComponents],
+  entryComponents: [...entryComponents],
+  providers: [...services],
+  exports: [...components, entryComponents]
 })
 export class NgxAuthenticationModule {}
