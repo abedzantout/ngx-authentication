a2e0e20d206ba997fbdf7bf08021802c75bb078e
Abed Zantout <abed@techhive.io>
2019-10-04 14:05:25 +0300

    feat(app): added different pages skeleton

--- projects/ngx-authentication/src/lib/ngx-authentication.module.ts
+++ projects/ngx-authentication/src/lib/ngx-authentication.module.ts
@@ -5,13 +5,26 @@ import { CommonModule } from '@angular/common';
 
 import { NgxAuthenticationComponent } from './ngx-authentication.component';
 import { AuthContainerComponent } from './containers/auth-container/auth-container.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { RegisterComponent } from './components/register/register.component';
+import { RequestPasswordComponent } from './components/request-password/request-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 const components = [NgxAuthenticationComponent, AuthContainerComponent];
 const services = [];
 const entryComponents = [];
 @NgModule({
   imports: [CommonModule, RouterModule, FormsModule],
-  declarations: [...components, ...entryComponents],
+  declarations: [
+    ...components,
+    ...entryComponents,
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent,
+    RequestPasswordComponent,
+    ResetPasswordComponent
+  ],
   entryComponents: [...entryComponents],
   providers: [...services],
   exports: [...components, entryComponents]
